3b47589052b9be020dda1ea7f003e860b3e036f9
Niharika Kara <niharikakara9@gmail.com>
2023-09-21 19:17:00 +0000

    Position class changes removed getPosition method

--- src/main/java/com/levelup/forestsandmonsters/Position.java
+++ src/main/java/com/levelup/forestsandmonsters/Position.java
@@ -10,9 +10,9 @@ public class Position {
         yCoordinate = this.yCoordinate;
     }
 
-    public Position getPosition(int xCoordinate, int yCoordinate){
+    /*public Position getPosition(int xCoordinate, int yCoordinate){
         Position position = new Position(xCoordinate, yCoordinate);
         return position;
-    }
+    }*/
 
 }
