5caa1c88a3b5f4e4fcb288028153000bad15b09e
James Travis <antares686@gmail.com>
2023-09-21 15:10:38 +0000

    Map now has move

--- src/main/java/com/levelup/forestsandmonsters/Map.java
+++ src/main/java/com/levelup/forestsandmonsters/Map.java
@@ -2,4 +2,7 @@ package com.levelup.forestsandmonsters;
 
 public class Map {
     
+    public void move(int xCoordinate, int yCoordinate) {
+
+    }
 }
--- src/test/java/com/levelup/forestsandmonsters/MapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/MapTest.java
@@ -1,5 +1,22 @@
 package com.levelup.forestsandmonsters;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class MapTest {
-    
+    @Test
+    public void test_map_init() {
+        Map map = new Map();
+        assertNotNull("Map object created", map);
+    }
+
+    @Test
+    public void test_map_move() {
+        Map map = new Map();
+        int xCoordinate = 0;
+        int yCoordinate = 0;
+
+        map.move(xCoordinate, yCoordinate);
+    }
 }
