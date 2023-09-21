d21fb506e90da14063080970ee5fd175e7ab233f
James Travis <antares686@gmail.com>
2023-09-21 15:41:46 +0000

    Map checking bad position start

--- src/main/java/com/levelup/forestsandmonsters/Map.java
+++ src/main/java/com/levelup/forestsandmonsters/Map.java
@@ -2,7 +2,12 @@ package com.levelup.forestsandmonsters;
 
 public class Map {
     
-    public int[] move(int xCoordinate, int yCoordinate) {
-        return new int[] { xCoordinate, yCoordinate};
+    public int[] move(int[] position) {
+        if(position[0] < 0 || position[1] < 0)
+        {
+            return new int[]{0,0};
+        }
+
+        return position;
     }
 }
--- src/test/java/com/levelup/forestsandmonsters/MapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/MapTest.java
@@ -15,11 +15,26 @@ public class MapTest {
     @Test
     public void test_map_move() {
         Map map = new Map();
-        int xCoordinate = 0;
-        int yCoordinate = 0;
+        
+        int[] position = new int[]{0, 0};
 
-        int[] returnPosition = map.move(xCoordinate, yCoordinate);
+        int[] returnPosition = map.move(position);
 
-        assertArrayEquals(new int[]{xCoordinate, yCoordinate}, returnPosition);
+        assertArrayEquals(position, returnPosition);
+    }
+
+    @Test
+    public void test_map_bad_move() {
+        Map map = new Map();
+        
+        int[] positionBadX = new int[]{-1, 0};
+        int[] positionBadY = new int[]{0, -1};
+        int[] expectedPosition = new int[]{0, 0};
+
+        int[] returnPositionX = map.move(positionBadX);
+        int[] returnPositionY = map.move(positionBadY);
+
+        assertArrayEquals(expectedPosition, returnPositionX);
+        assertArrayEquals(expectedPosition, returnPositionY);
     }
 }
