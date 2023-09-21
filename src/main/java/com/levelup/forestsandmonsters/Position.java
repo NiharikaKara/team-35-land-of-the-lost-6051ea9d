54d6c6ef6a859ead8839dad3811a9b4301833ce0
Niharika Kara <niharikakara9@gmail.com>
2023-09-21 15:18:26 +0000

    Position Class

--- src/main/java/com/levelup/forestsandmonsters/Position.java
+++ src/main/java/com/levelup/forestsandmonsters/Position.java
@@ -1,8 +1,18 @@
 package com.levelup.forestsandmonsters;
 
 public class Position {
+    
+    int xCoordinate;
+    int yCoordinate;
 
-    public Position() {
+    public Position(int xCoordinate, int yCoordinate) {
+        xCoordinate = this.xCoordinate;
+        yCoordinate = this.yCoordinate;
+    }
+
+    public Position getPosition(int xCoordinate, int yCoordinate){
+        Position position = new Position(xCoordinate, yCoordinate);
+        return position;
     }
 
 }
