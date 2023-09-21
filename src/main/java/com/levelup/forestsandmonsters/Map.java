e288f2ac5449e872c9418e139ed2f418f8a76ab1
James Travis <antares686@gmail.com>
2023-09-21 19:42:46 +0000

    Map grid 10 by 10

--- src/main/java/com/levelup/forestsandmonsters/Map.java
+++ src/main/java/com/levelup/forestsandmonsters/Map.java
@@ -1,11 +1,29 @@
 package com.levelup.forestsandmonsters;
 
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map {
     public int size;
+    private List<Point> tiles = new ArrayList<>();
     
     public Map(int columns, int rows) {
-        this.size = 16;
+        
+        for(int x=0; x<columns; x++) {
+            for (int y=0; y<rows; y++) {
+                this.tiles.add(new Point(x,y));
+            }
+        }
+
+        this.size = tiles.size();
     }
+
+    public boolean isValidPosition(Point checkValue) {
+        return this.tiles.contains(checkValue);
+    }
+    
 
     public int[] move(int[] position) {
         if(position[0] < 0 || position[1] < 0)
--- src/test/java/com/levelup/forestsandmonsters/MapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/MapTest.java
@@ -2,7 +2,11 @@ package com.levelup.forestsandmonsters;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
 
 import org.junit.Test;
 
@@ -10,14 +14,26 @@ public class MapTest {
     
     @Test
     public void test_map_init() {
-        Map map = new Map(4,4);
+        int columns = 10;
+        int rows = 10;
+
+        Map map = new Map(columns, rows);
         assertNotNull("Map object created", map);
-        assertEquals(16, map.size);
+        assertEquals((columns * rows), map.size);
+
+        assertFalse(map.isValidPosition(new Point(-1,0)));
+        assertFalse(map.isValidPosition(new Point(0,-1)));
+        assertFalse(map.isValidPosition(new Point(10,0)));
+        assertFalse(map.isValidPosition(new Point(0,10)));
+        assertTrue(map.isValidPosition(new Point(0,0)));
+        assertTrue(map.isValidPosition(new Point(0,9)));
+        assertTrue(map.isValidPosition(new Point(9,0)));
+        assertTrue(map.isValidPosition(new Point(9,9)));
     }
 
     @Test
     public void test_map_move() {
-        Map map = new Map(4,4);
+        Map map = new Map(10,10);
         
         int[] position = new int[]{0, 0};
 
@@ -28,7 +44,7 @@ public class MapTest {
 
     @Test
     public void test_map_bad_move() {
-        Map map = new Map(4,4);
+        Map map = new Map(10,10);
         
         int[] positionBadX = new int[]{-1, 0};
         int[] positionBadY = new int[]{0, -1};
