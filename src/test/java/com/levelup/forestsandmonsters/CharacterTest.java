89ba5f0bf74a813f557a19cf90b1e765f4b554a4
Nuba J <nubaj7@gmail.com>
2023-09-21 15:50:03 +0000

    added CharacterTest code broken need help

--- src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
+++ src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
@@ -1,15 +1,18 @@
 package com.levelup.forestsandmonsters;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class CharacterTest {
 
+    @Test
     public void testDefaultCharacterName()
     {
     Character character = new Character();
     Assert.assertEquals(character.getName(), character.DEFAULT_CHARACTER_NAME);
     }
 
+    @Test
     public void testCharacterName(String val) 
     {
         Character character = new Character("JimBob");
