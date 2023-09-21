a6693e045dc79bbd9239d7f46e14163531f20592
Niharika Kara <niharikakara9@gmail.com>
2023-09-21 15:23:00 +0000

    Renamed Position Test file

--- src/test/java/com/levelup/forestsandmonsters/PositionTest.java
+++ src/test/java/com/levelup/forestsandmonsters/PositionTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class PositionTest {
     @Test
     public void InitializationCreatesResults() {
-        Position testObj = new Position();
+        Position testObj = new Position(int);
         assertNotNull(testObj.status);
     }
 }
